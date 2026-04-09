264a2273467b3d9329661da77365fcea87d3e781
Deependra-Das <iamdeep75@gmail.com>
2026-04-09 18:27:16 +0530

    Changes for testing Player Animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _groundDistance = 0.2f;
 
     [Header("References")]
+    [SerializeField] private Animator _animator;
     [SerializeField] private Transform _cameraTransform;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private LayerMask _groundMask;
@@ -29,6 +30,12 @@ public class PlayerController : MonoBehaviour
     private bool _isGrounded;
     private bool _wasGrounded;
 
+    private int _speedHash;
+    private int _groundedHash;
+    private int _verticalHash;
+    private int _jumpStartTrigger;
+    private int _jumpLandTrigger;
+
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
@@ -43,6 +50,13 @@ public class PlayerController : MonoBehaviour
         _jumpAction.performed += ctx => Jump();
         _lookAction.performed += ctx => _lookInput = ctx.ReadValue<Vector2>();
         _lookAction.canceled += ctx => _lookInput = Vector2.zero;
+
+
+        _speedHash = Animator.StringToHash("Speed");
+        _groundedHash = Animator.StringToHash("IsGrounded");
+        _verticalHash = Animator.StringToHash("VerticalVelocity");
+        _jumpStartTrigger = Animator.StringToHash("JumpStart");
+        _jumpLandTrigger = Animator.StringToHash("JumpLand");
     }
 
     void OnEnable()
@@ -64,6 +78,12 @@ public class PlayerController : MonoBehaviour
         GroundCheck();
         HandleMovement();
         ApplyGravity();
+        UpdateAnimations();
+
+        if (!_wasGrounded && _isGrounded)
+        {
+            _animator.SetTrigger(_jumpLandTrigger);
+        }
 
         _wasGrounded = _isGrounded;
     }
@@ -88,6 +108,7 @@ public class PlayerController : MonoBehaviour
         if (_isGrounded)
         {
             _velocity.y = _jumpForce;
+            _animator.SetTrigger(_jumpStartTrigger);
         }
     }
 
@@ -108,6 +129,22 @@ public class PlayerController : MonoBehaviour
     void GroundCheck()
     {
         _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+    }
+
+    void UpdateAnimations()
+    {
+        Vector3 camForward = Vector3.ProjectOnPlane(_cameraTransform.forward, Vector3.up).normalized;
+        Vector3 camRight = Vector3.Cross(Vector3.up, camForward);
+        Vector3 move = camForward * _moveInput.y + camRight * _moveInput.x;
+
+        float speed = move.magnitude;
+        float smoothSpeed = Mathf.Lerp(_animator.GetFloat(_speedHash), speed, 10f * Time.deltaTime);
+        _animator.SetFloat(_speedHash, smoothSpeed);
+
+        bool groundedForAnim = _isGrounded && _velocity.y <= 0;
+        _animator.SetBool(_groundedHash, groundedForAnim);
+
+        _animator.SetFloat(_verticalHash, _velocity.y);
     }
 
     void OnDrawGizmosSelected()
